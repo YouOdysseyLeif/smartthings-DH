f2e9edd5d26459cf2b00b68ee015fd9c505c6c17
Leif <leif@lightpalettestudios.com>
2019-07-07 23:27:32 -0600

    initial copy

---
+++ zig_smart_scene_dude.groovy
@@ -0,0 +1,9 @@
+	fingerprint profileId: "0104",
+		deviceId: "000C",
+		inClusters: "0000, 0005",
+		outClusters: "0000, 0005, 0017",
+		manufacturer: "欧瑞博",
+		model: "75d430d66c164c26ac8601c05932dc94",
+		deviceJoinName: "7 Button Scene Controller"
+
+        
